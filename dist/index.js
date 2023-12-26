ea43316ddd82b9c54b3df8e9940e0a0ade3b3788
Student-AKM <ankitkrmaurya82@gmail.com>
2023-12-26 20:55:41 +0530

    bug fix

--- dist/index.js
+++ dist/index.js
@@ -11,3 +11,30 @@ function toggleHide(){
 
 
 
+let title = document.getElementById("title");
+let user = document.getElementById("user");
+
+// console.log((title),(user));
+
+let prof = {
+    User: {
+        userName:{
+            firstName: "Hitesh",
+            lastName: "Chaudhary"
+        },
+        occup: "JS Developer",
+    }
+
+}
+
+// console.log(prof.User.occup);
+
+
+title.innerHTML = prof.User.occup;
+
+let FirstName = prof.User.userName.firstName;
+let LastName = prof.User.userName.lastName;
+
+console.log(`${FirstName} ${""}${LastName}`);
+let FullName = `${FirstName} ${""}${LastName}`;
+user.innerHTML = `${FirstName} ${""}${LastName}`;
